4c4e84fb897c90a20acd40dd6eb2180db46aceef
alexDemche <alexeyydemchenko@gmail.com>
2019-09-12 17:46:39 +0300

    Updates

---
+++ precache-manifest.3df740a3cb94cf92d89605fa920aa3cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85cdab1e68fa1dc2a337836aad4565fc",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "8852ca476d6dd2873bca",
+    "url": "/react_tabs/static/css/main.6bb0ebc6.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/react_tabs/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "8852ca476d6dd2873bca",
+    "url": "/react_tabs/static/js/main.232664e7.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
